c9a84a8f927b7efc7be44e6a2a88ef54def5b0f1
Robert DeJong <radj@hotmail.ca>
2021-08-11 10:17:48 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Synthesis;
+using Mutagen.Bethesda.Skyrim;
+using System.Threading.Tasks;
+
+namespace AATPatcher
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            return await SynthesisPipeline.Instance
+                .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
+                .SetTypicalOpen(GameRelease.SkyrimSE, "YourPatcher.esp")
+                .Run(args);
+        }
+
+        public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+
+            //Your code here!
+        }
+    }
+}
