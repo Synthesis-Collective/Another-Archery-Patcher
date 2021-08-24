38d0f4e015eff3fde55636d2e058d556dfc57d89
radj307 <radj@hotmail.ca>
2021-08-24 15:11:16 -0400

    Replaced string lists with MatchableElement list

--- Another_Archery_Patcher/settings.cs
+++ Another_Archery_Patcher/settings.cs
@@ -54,45 +54,106 @@ namespace Another_Archery_Patcher
         [SettingName("Patch Trap Projectiles"), Tooltip("Modifies most of the projectiles fired by dart traps & dwemer ballista traps to be more interesting. (If anyone wants to customize the values, let me know and I'll add it)")]
         public bool PatchTraps;
     }
+
+    public class MatchableElement
+    {
+        protected bool Equals(MatchableElement other)
+        {
+            return Name == other.Name && Required == other.Required;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((MatchableElement)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Required);
+        }
+
+        public MatchableElement(string name, bool required = false)
+        {
+            Name = name;
+            Required = required;
+        }
+        [MaintainOrder]
+        [Tooltip("This is the string to search for in each Projectile's Editor ID (Or Form ID)")]
+        public string Name;
+        [Tooltip("When true, a projectile is required to match this entry to be considered part of this category, even if it matches other non-required entries too.")]
+        public bool Required;
+
+        public bool IsMatch(string? id)
+        {
+            if (id == null) return false;
+            return Name == id || id.Contains(Name, StringComparison.OrdinalIgnoreCase) || Name.Contains(id, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public static bool operator ==(MatchableElement elem, string comp)
+        {
+            return elem.Name == comp;
+        }
+        public static bool operator !=(MatchableElement elem, string comp)
+        {
+            return elem.Name == comp;
+        }
+    }
     /**
      * @class Matchable
      * @brief Base class that allows disabling itself, and exposes a list of strings used for matching records against it.
      */
     public class Matchable
     {
-        public Matchable(List<string>? matchlist, bool enabled = true)
+        public Matchable(List<MatchableElement>? matchlist)
         {
-            Enabled = enabled;
-            Matchlist = matchlist ?? (new List<string>());
+            Matchlist = matchlist ?? new List<MatchableElement>();
+        }
+
+        private bool ContainsRequired(string? id)
+        {
+            return id != null && Matchlist.All(elem => !elem.Required || elem.Required && elem.IsMatch(id));
+        }
+        private bool Contains(string? id)
+        {
+            return id != null && Matchlist.Any(elem => elem.IsMatch(id)) && ContainsRequired(id);
         }
         /**
          * @brief Checks if the given Editor ID or Form ID is a case-insensitive match for any of the strings in the matchlist.
          */
-        public bool IsMatch(string? id, bool allowPartialMatch = true)
+        public bool IsMatch(string? id)
         {
-            if (id == null || !Enabled || Matchlist.Count <= 0) return false;
-            return !allowPartialMatch ? Matchlist.Contains(id) : Matchlist.Any(comp => id.Contains(comp, StringComparison.OrdinalIgnoreCase) || comp == id);
+            if (id == null || Matchlist.Count <= 0) return false;
+            return Contains(id);
         }
         [MaintainOrder]
-        [SettingName("Enable"), JsonDiskName("enabled")]
-        public bool Enabled;
         [SettingName("Common Names"), JsonDiskName("matchlist"), Tooltip("(Don't change this unless you know what you're doing!) Used to resolve projectile type, as there is no other way to distinguish between arrows/bolts/other")]
-        public List<string> Matchlist;
+        public List<MatchableElement> Matchlist;
     }
     /**
      * @class MatchableRecord
      * @brief Represents a list of Records that are used for the record blacklisting feature.
      */
-    public class MatchableRecord : Matchable
+    public class MatchableBlacklist : Matchable
     {
         /**
          * @brief Default constructor that takes a list of blacklisted IDs & list of blacklisted Record FormLinks
          */
-        public MatchableRecord(List<string> blacklistedIDs, List<IFormLinkGetter<IProjectileGetter>> blacklistedRecords) : base(blacklistedIDs)
+        public MatchableBlacklist(bool enabled, List<MatchableElement> blacklistedIDs, List<IFormLinkGetter<IProjectileGetter>> blacklistedRecords) : base(blacklistedIDs)
         {
+            Enabled = enabled;
             Record = blacklistedRecords;
         }
+
         [MaintainOrder]
+        [Tooltip("Disabling this will disable this blacklist.")]
+        public bool Enabled;
         [SettingName("Records")]
         public List<IFormLinkGetter<IProjectileGetter>> Record;
     }
@@ -131,28 +192,31 @@ namespace Another_Archery_Patcher
         /**
          * @brief Default constructor that takes all parameters required by both the Matchable class & ProjectileStats class.
          */
-        public ProjectileTweaks(bool enable, float projSpeed, float projGravity, float projImpactForce, SoundLevel projSoundLevel, List<string>? matchableIds = null) : base(matchableIds, enable)
+        public ProjectileTweaks(float projSpeed, float projGravity, float projImpactForce, SoundLevel projSoundLevel, List<MatchableElement>? matchableIds = null) : base(matchableIds)
         {
             Stats = new ProjectileStats(projSpeed, projGravity, projImpactForce, projSoundLevel);
         }
         [MaintainOrder]
         [SettingName("Stats")]
-        public ProjectileStats Stats; ///< @brief Contains the values to be applied to projectiles in this group.
+        public ProjectileStats Stats;
+
+        ///< @brief Contains the values to be applied to projectiles in this group.
+
+        public string GetMatchlistAsString()
+        {
+            return Matchlist.Aggregate("", (current, match) => current + (match.ToString() + ", "));
+        }
 
         public string GetVarsAsString()
         {
             string str = "( ";
-            if (Enabled)
-            {
-                str += "Speed:" + Stats.Speed + ", ";
-                str += "Gravity:" + Stats.Gravity + ", ";
-                str += "ImpactForce:" + Stats.ImpactForce + ", ";
-                str += "SoundLevel:" + Stats.SoundLevel;
-                str += "Matchlist:[ ";
-                str += Matchlist.Aggregate(str, (current, match) => current + (match + ", "));
-                str += " ]";
-            }
-            else str += "[Disabled]";
+            str += "Speed:" + Stats.Speed + ", ";
+            str += "Gravity:" + Stats.Gravity + ", ";
+            str += "ImpactForce:" + Stats.ImpactForce + ", ";
+            str += "SoundLevel:" + Stats.SoundLevel + ", ";
+            str += "Matchlist:[ ";
+            str += Matchlist.Aggregate(str, (current, match) => current + (match.ToString() + ", "));
+            str += " ]";
             return str + " )";
         }
     }
@@ -170,12 +234,12 @@ namespace Another_Archery_Patcher
         [SettingName("Projectile Tweaks"), Tooltip("Various categories of projectiles to patch.")]
         public List<ProjectileTweaks> ProjectileTweaks = new()
         {
-            new ProjectileTweaks(true, 5000.0f, 0.34f, 0.44f, Silent, new List<string> { "Arrow" }), ///< @brief Values that are applied to Arrows.
-            new ProjectileTweaks(true, 5800.0f, 0.34f, 0.64f, Normal, new List<string> { "Bolt" }), ///< @brief Values that are applied to Bolts.
-            new ProjectileTweaks(true, 2800.0f, 0.13f, 1.1f, Silent, new List<string> { "Riekling", "SSM", "Throw" }) ///< @brief Values that are applied to throwable weapons, like spears. (In vanilla, this is only applied to the Riekling Spear.)
+            new ProjectileTweaks(5000.0f, 0.34f, 0.44f, Silent, new List<MatchableElement> { new("Arrow") }), ///< @brief Values that are applied to Arrows.
+            new ProjectileTweaks(5800.0f, 0.34f, 0.64f, Normal, new List<MatchableElement> { new("Bolt") }), ///< @brief Values that are applied to Bolts.
+            new ProjectileTweaks(2800.0f, 0.13f, 1.1f, Silent, new List<MatchableElement> { new("Riekling"), new("SSM"), new("Throw") }) ///< @brief Values that are applied to throwable weapons, like spears. (In vanilla, this is only applied to the Riekling Spear.)
         };
         [SettingName("Blacklist"), Tooltip("Any projectiles specified here will not be modified.")]
-        public MatchableRecord Blacklist = new(new List<string> { "MQ101ArrowSteelProjectile" }, new List<IFormLinkGetter<IProjectileGetter>>()); ///< @brief Used to ignore certain projectiles used in quests / other projectiles that are sensitive to modification.
+        public MatchableBlacklist Blacklist = new(true, new List<MatchableElement> { new("MQ101ArrowSteelProjectile") }, new List<IFormLinkGetter<IProjectileGetter>>()); ///< @brief Used to ignore certain projectiles used in quests / other projectiles that are sensitive to modification.
         [SettingName("Verbose Log"), JsonDiskName("verbose-log"), Tooltip("Writes additional information to the log, useful for debugging.")]
         public bool UseVerboseLog = true; ///< @brief Toggles verbose console logging.
     }
