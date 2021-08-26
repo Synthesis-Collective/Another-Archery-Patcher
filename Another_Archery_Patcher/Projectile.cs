4d2a283bdebd05d95e12a8684b184fd3c5a3bf53
radj307 <radj@hotmail.ca>
2021-08-26 10:56:46 -0400

    Removed unused using directives

--- Another_Archery_Patcher/Projectile.cs
+++ Another_Archery_Patcher/Projectile.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Mutagen.Bethesda.WPF.Reflection.Attributes;
 
 namespace Another_Archery_Patcher
