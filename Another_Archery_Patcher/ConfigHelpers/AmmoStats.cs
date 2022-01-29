db06f4f9e92f8af3913302e9539faebc96afe487
radj307 <radj307@gmail.com>
2022-01-29 17:46:37 -0500

    replace get- functions in AmmoStats.cs to fix #2

--- Another_Archery_Patcher/ConfigHelpers/AmmoStats.cs
+++ Another_Archery_Patcher/ConfigHelpers/AmmoStats.cs
@@ -39,53 +39,71 @@ namespace Another_Archery_Patcher.ConfigHelpers
         public bool WeightIsModifier;
         public float Weight;
 
+        /// <summary>
+        /// Retrieve the modified damage of an AMMO record, according to the user's configuration.
+        /// Also checks if the value was modified to avoid ITPOs & count changes.
+        /// </summary>
+        /// <param name="current">The current damage of an AMMO record.</param>
+        /// <param name="changed">An output variable that when true, indicates that the record is not an ITPO and should be added to the patch.</param>
+        /// <returns>(float): The modified damage.</returns>
         public float GetDamage(float current, out bool changed)
         {
-            if (DamageIsModifier)
-            {
-                changed = !Damage.EqualsWithin(0);
-                return changed ? current + Damage : current;
-            }
-            else
-            {
-                changed = current.EqualsWithin(Damage);
-                return changed ? Damage : current;
-            }
+            float newDamage = DamageIsModifier
+                ? (current + Damage)
+                : Damage;
+            // change when the current value is not equal to the modified value
+            changed = !current.EqualsWithin(newDamage);
+            return changed ? newDamage : current;
         }
 
+        /// <summary>
+        /// Retrieve the modified value of an AMMO record, according to the user's configuration.
+        /// Also checks if the value was modified to avoid ITPOs & count changes.
+        /// </summary>
+        /// <param name="current">The current value of an AMMO record.</param>
+        /// <param name="changed">An output variable that when true, indicates that the record is not an ITPO and should be added to the patch.</param>
+        /// <returns>(float): The modified value.</returns>
         public uint GetValue(uint current, out bool changed)
         {
-            if (ValueIsModifier)
-            {
-                changed = !Value.Equals(0);
-                return changed ? Value : current;
-            }
-            else
-            {
-                changed = current.Equals(Value);
-                return changed ? Value : current;
-            }
+            uint newValue = ValueIsModifier
+                ? (current + Value)
+                : Value;
+            // change when the current value is not equal to the modified value
+            changed = !current.Equals(newValue);
+            return changed ? newValue : current;
         }
 
+        /// <summary>
+        /// Retrieve the modified weight of an AMMO record, according to the user's configuration.
+        /// Also checks if the value was modified to avoid ITPOs & count changes.
+        /// </summary>
+        /// <param name="current">The current weight</param>
+        /// <param name="changed">An output variable that when true, indicates that the record is not an ITPO and should be added to the patch.</param>
+        /// <returns>(float): The modified weight.</returns>
         public float GetWeight(float current, out bool changed)
         {
-            if (WeightIsModifier)
-            {
-                changed = !Weight.EqualsWithin(0);
-                return changed ? Weight + current : current;
-            }
-            else
-            {
-                changed = current.EqualsWithin(Weight);
-                return changed ? Weight : current;
-            }
+            float newWeight = WeightIsModifier
+                ? (current + Weight)
+                : Weight;
+            // change when the current value is not equal to the modified value
+            changed = !current.EqualsWithin(newWeight);
+            return changed ? newWeight : current;
         }
 
+        /// <summary>
+        /// Check if this entry should be skipped by checking if there are any values that aren't modifiers set to 0.
+        /// </summary>
+        /// <returns>(bool): True when there are valid values to apply to an AMMO record.</returns>
         public bool ShouldSkip()
         {
             return Damage.EqualsWithin(0) && DamageIsModifier && Value.Equals(0) && ValueIsModifier && Weight.EqualsWithin(0) && WeightIsModifier;
         }
 
+        /// <summary>
+        /// Accepts an Ammunition object, modifies it, and returns the modified copy as well as the number of changes.
+        /// </summary>
+        /// <param name="ammo">An Ammunition object, retrieved by performing a deep copy of a record.</param>
+        /// <returns>((Ammunition): The modified ammunition object,(int): The number of changes made to the object.)</returns>
         public (Ammunition, int) ApplySettingsTo(Ammunition ammo)
         {
             var count = 0;
