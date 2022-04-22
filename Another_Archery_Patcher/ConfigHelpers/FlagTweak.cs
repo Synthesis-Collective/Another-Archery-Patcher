d113d0d5248fbd1ca16017cba742e3721e319114
radj307 <radj307@gmail.com>
2022-04-21 20:47:36 -0400

    revert changes to the default branch

--- Another_Archery_Patcher/ConfigHelpers/FlagTweak.cs
+++ Another_Archery_Patcher/ConfigHelpers/FlagTweak.cs
@@ -1,49 +1,27 @@
-using Another_Archery_Patcher.ConfigHelpers.RecordEditor;
-using Mutagen.Bethesda.Skyrim;
 using Mutagen.Bethesda.WPF.Reflection.Attributes;
-using Noggog;
 
 namespace Another_Archery_Patcher.ConfigHelpers
 {
-    /// <summary>
-    /// Contains a <see cref="ProjectileFlag.Type"/> and a <see cref="ProjectileFlag.State"/>.
-    /// </summary>
-    /// <remarks>
-    /// This is used instead of a Dictionary because I don't like how Dictionaries look in the settings menu.
-    /// </remarks>
+    /**
+     * @class FlagTweak
+     * @brief Contains a projectile flag, and a FlagState. This is used instead of a Dictionary because I don't like how Dictionaries look in the settings menu.
+     */
     [ObjectNameMember(nameof(Flag))]
     public class FlagTweak
     {
-        public FlagTweak(ProjectileFlag.Flag flag, ProjectileFlag.State state)
+        public FlagTweak(RecordEdit.Editor.Flag.Type flag, RecordEdit.Editor.Flag.State state)
         {
             Flag = flag;
             State = state;
         }
 
-        public ProjectileFlag.Flag Flag;
-        public ProjectileFlag.State State;
+        public RecordEdit.Editor.Flag.Type Flag;
+        public RecordEdit.Editor.Flag.State State;
 
-        /// <summary>
-        /// Allows the <see cref="FlagTweak"/> type to be deconstructed into a tuple.
-        /// </summary>
-        /// <param name="flag">Item1</param>
-        /// <param name="state">Item2</param>
-        public void Deconstruct(out ProjectileFlag.Flag flag, out ProjectileFlag.State state)
+        public void Deconstruct(out RecordEdit.Editor.Flag.Type flag, out RecordEdit.Editor.Flag.State state)
         {
             flag = Flag;
             state = State;
-        }
-
-        public Projectile ApplyTo(Projectile proj)
-        {
-            proj.Flags.SetFlag((Projectile.Flag)Flag, State == ProjectileFlag.State.Add);
-            return proj;
-        }
-
-        public Projectile ApplyTo(Projectile proj, out bool changed)
-        {
-            changed = proj.Flags != proj.Flags.SetFlag((Projectile.Flag)Flag, State == ProjectileFlag.State.Add);
-            return proj;
         }
     }
 }
--- Another_Archery_Patcher/settings.cs
+++ Another_Archery_Patcher/settings.cs
@@ -1,5 +1,5 @@
 using Another_Archery_Patcher.ConfigHelpers;
-using Another_Archery_Patcher.ConfigHelpers.RecordEditor;
+using Another_Archery_Patcher.ConfigHelpers.RecordEdit;
 using Mutagen.Bethesda.FormKeys.SkyrimSE;
 using Mutagen.Bethesda.Plugins;
 using Mutagen.Bethesda.Skyrim;
@@ -10,19 +10,16 @@ using System.Linq;
 
 namespace Another_Archery_Patcher
 {
-    using static ProjectileFlag.State;
-    using static ProjectileFlag.Flag;
+    using static Editor.Flag.State;
+    using static Editor.Flag.Type;
 
-    /// <summary>
-    /// Top-level settings object that stores all of the patcher's settings.
-    /// </summary>
     public class Settings : StatsPresets
     {
         [MaintainOrder]
         [SettingName("Global Flag Tweaks")] // FLAG TWEAKS
-        public FlagTweakList GlobalFlagTweaks = new()
+        public List<FlagTweak> GlobalFlagTweaks = new()
         {
-            new(Supersonic, Remove),
+            new FlagTweak(Supersonic, Remove)
         };
 
         [SettingName("Game Settings"), Tooltip("Change the value of archery-related game settings.")] // GAME SETTINGS
@@ -34,10 +31,6 @@ namespace Another_Archery_Patcher
             Skyrim.Projectile.MQ101ArrowSteelProjectile,
         };
 
-        /// <summary>
-        /// Used to determine which projectile types the patcher supports.
-        /// This is not exposed to the end user.
-        /// </summary>
         private static readonly Dictionary<Projectile.TypeEnum, (bool, List<string>?)> ProjectileTypeWhitelist = new()
         {
             { Projectile.TypeEnum.Arrow, (true, null) },
@@ -46,27 +39,13 @@ namespace Another_Archery_Patcher
 
         // FUNCTIONS
 
-        /// <summary>
-        /// Check if the given <see cref="FormKey"/> is present on the blacklist.
-        /// </summary>
-        /// <param name="projFormKey">FormKey to check.</param>
-        /// <returns><list type="table">
-        /// <item><term>true</term><description>Given FormKey is blacklisted, and should be ignored.</description></item>
-        /// <item><term>false</term><description>Given FormKey isn't blacklisted.</description></item>
-        /// </list></returns>
+        // Check if a given formkey is on the blacklist
         private bool IsBlacklisted(FormKey projFormKey)
         {
             return Blacklist.Any(blacklisted => blacklisted.FormKey == projFormKey);
         }
 
-        /// <summary>
-        /// Check if the given <see cref="IProjectileGetter"/> is a valid target for the patcher by checking the <see cref="FormKey"/> blacklist.
-        /// </summary>
-        /// <param name="projGetter">Projectile record getter to check.</param>
-        /// <returns><list type="table">
-        /// <item><term>true</term><description>Projectile record should be processed.</description></item>
-        /// <item><term>false</term><description>Projectile record should be ignored.</description></item>
-        /// </list></returns>
+        // Check if a given projectile is a valid target for the patcher
         public bool IsValidPatchTarget(IProjectileGetter projGetter)
         {
             if (projGetter.EditorID == null)
@@ -79,49 +58,32 @@ namespace Another_Archery_Patcher
             return false;
         }
 
-        /// <summary>
-        /// Apply the stats from the projectile category with the highest priority.
-        /// </summary>
-        /// <param name="proj">A <see cref="Projectile"/> record to modify.</param>
-        /// <returns>A tuple with the following:
-        /// <list type="table">
-        /// <item><term>Item1</term><description>The modified <see cref="Projectile"/> record.</description></item>
-        /// <item><term>Item2</term><description>The number of changed subrecords.</description></item>
-        /// <item><term>Item3</term><description>The identifier of the stats category that was applied to the projectile.</description></item>
-        /// </list></returns>
+        // Apply the highest priority stats to a given projectile, if it isn't blacklisted and a valid category was found
         public (Projectile, uint, string) ApplyHighestPriorityStats(Projectile proj)
         {
-            if (proj.EditorID == null)
-                return (proj, 0, "");
-            var stats = GetHighestPriorityStats(proj.EditorID);
+            var stats = GetHighestPriorityStats(proj);
             var countChanges = 0u;
             if (stats == null)
                 return (proj, countChanges, "[NULL]");
-
             var identifier = stats!.Identifier;
-
             // Speed
             proj.Speed = stats.GetSpeed(proj.Speed, out var changed);
             countChanges += changed ? 1u : 0u;
-
             // Gravity
             proj.Gravity = stats.GetGravity(proj.Gravity, out changed);
             countChanges += changed ? 1u : 0u;
-
             // Impact Force
             proj.ImpactForce = stats.GetImpactForce(proj.ImpactForce, out changed);
             countChanges += changed ? 1u : 0u;
-
             // Sound Level
             proj.SoundLevel = stats.GetSoundLevel(proj.SoundLevel, out changed);
             countChanges += changed ? 1u : 0u;
-
             // Flags
-            proj = GlobalFlagTweaks.ApplyTo(proj, out uint globalFlagChanges);
-            countChanges += globalFlagChanges;
-            proj = stats.Flags.ApplyTo(proj, out uint statFlagChanges);
-            countChanges += statFlagChanges;
-
+            uint countFlagChanges;
+            (proj, countFlagChanges) = Editor.Flag.ApplyFlags(proj, GlobalFlagTweaks); // apply global flags
+            countChanges += countFlagChanges;
+            (proj, countFlagChanges) = Editor.Flag.ApplyFlags(proj, stats.Flags); // apply specific flags
+            countChanges += countFlagChanges;
             return (proj, countChanges, identifier);
         }
     }
